e5bbb18a9128acdb9931438f207f6c64728ed300
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2024-04-01 20:53:12 -0700

    add the PhotolistItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,21 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+const PhotoListItem = (props) => {
+  const { id, location, imageSource, username, profile } = props.data;
 
-const PhotoListItem = () => {
-  /* Insert React */
+  return (
+    <div className="photo-list-item">
+      <img src={imageSource} alt={`Photo ny ${username}`} />
+      <div className="details">
+        <h2>{username}</h2>
+        <p>
+          {location.city}, {location.country}
+        </p>
+      </div>
+      <img className="profile" src={profile} alt={`Profile of ${username}`} />
+    </div>
+  );
 };
 
 export default PhotoListItem;
