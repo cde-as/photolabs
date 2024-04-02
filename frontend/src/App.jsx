4e7a6b91062a0da982485d6206da7fc1daea96f2
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2024-04-01 22:02:39 -0700

    Display 3 different photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,8 @@ import PhotoListItem from './components/PhotoListItem';
 import PhotoList from './components/PhotoList';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
+const sampleDataForPhotoListItem = [
+  {
   id: "1",
   location: {
     city: "Montreal",
@@ -13,13 +14,42 @@ const sampleDataForPhotoListItem = {
   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
   username: "Joe Example",
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+  },
+
+  {
+  id: "2",
+  location: {
+    city: "Toronto",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  },
+
+  {
+  id: "3",
+  location: {
+    "city": "Ottawa",
+    "country": "Canada"
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  }
+];
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem data={sampleDataForPhotoListItem} />
+
+      <div className="photo-list">
+      {sampleDataForPhotoListItem.map(photoData => (
+          <PhotoListItem key={photoData.id} data={photoData} photoId={photoData.id} />
+      ))}
+      </div>
+
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -10,13 +10,19 @@ const PhotoListItem = (props) => {
   return (
     <div className="photo-list__item" key={photoId}>
       <img src={imageSource} alt={`Photo ny ${username}`} />
-      <div className="details">
-        <h2>{username}</h2>
-        <p>
-          {location.city}, {location.country}
-        </p>
+      
+      <div className="photo-list__user-info photo-list__user-details">
+      <img className="photo-list__user-profile" src={profile} alt={`Profile of ${username}`} />
+       
+        <h2 className="photo-list__user-info photo-list__user-details">{username}</h2>
+
+        <div className="photo-list__user-location">
+          <p>
+            {location.city}, {location.country}
+          </p>
+        </div>
+
       </div>
-      <img className="profile" src={profile} alt={`Profile of ${username}`} />
     </div>
   );
 };
